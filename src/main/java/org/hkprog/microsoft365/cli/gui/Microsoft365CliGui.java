58938c9bb9441241b124ff252155fce58e05f8b8
Peter <peter@quantr.hk>
2025-06-24 20:33:43 +0800

    dev

---
+++ src/main/java/org/hkprog/microsoft365/cli/gui/Microsoft365CliGui.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.hkprog.microsoft365.cli.gui;
+
+/**
+ *
+ * @author Peter <peter@quantr.hk>
+ */
+public class Microsoft365CliGui {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
